5e109309249c410730a9b2f18c1b1398a7f17057
Vansh Chavda <vanshchavda111@gmail.com>
2024-05-28 20:35:34 +0530

    Adding vercel analytics

--- app/page.tsx
+++ app/page.tsx
@@ -11,11 +11,13 @@ import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
+
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
+
         <Hero />
         <Grid />
         <RecentProjects />
