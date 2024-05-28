10169d72a5401e182f821d1e85616a889c33953d
Vansh Chavda <vanshchavda111@gmail.com>
2024-05-28 19:57:06 +0530

    Fixed Errors

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Vortex } from "../components/ui/Vortex";
+import { Vortex } from "../components/ui/vortex";
 import MagicButton from "./MagicButton";
 import { FaLocationArrow } from "react-icons/fa6";
 import { socialMedia } from "@/data";
