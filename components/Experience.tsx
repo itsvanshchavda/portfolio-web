d0e226f594e2b7fc330fbbd9c419f82b67c9cdb4
Vansh Chavda <vanshchavda111@gmail.com>
2024-11-07 18:26:36 +0530

    Update Experience component for responsive design and image styling

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -69,7 +69,7 @@ function Experience() {
               @Yash Gohel
             </a> who is also a developer, and we turning ideas into reality and working on several projects together.
           </p>
-          <div className="grid grid-cols-2 gap-4">
+          <div className="grid grid-cols-2 max-sm:grid-cols-1 gap-4">
 
             <Image
               src="/setup.jpeg"
@@ -85,7 +85,7 @@ function Experience() {
               alt="feature"
               width={500}
               height={500}
-              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+              className="rounded-lg object-cover h-auto md:h-auto lg:h-auto w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
 
           </div>
